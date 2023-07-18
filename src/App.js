e6aa06c27fe3879b9611ba9383414ce0160acd92
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-18 12:26:03 -0400

    add switch case and if() code option

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Clock from "./Clock";
 
 function App(props) {
-  // write your code here
-  // hint: It's not necessary to pass props to clock as an argument.
   const { loggedIn } = props;
-  
+  if(loggedIn === true) {
+    return <Clock />;
+  } else {
+    return null;
+  }
 }
 
 export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,7 +1,34 @@
 import React from "react";
 
+
+//for switch case make sure to declare variable to return and add breaks between each case
 function Clock() {
-  // write your code here
-}
+  const currentTime = new Date().getHours();
+  let greeting;
+  
+  switch(true) {
+    case currentTime < 12:
+      greeting = <p>Good Morning!</p>;
+      break;
+    case currentTime < 18:
+      greeting = <p>Good Afternoon!</p>;
+      break;
+    default:
+      greeting = <p>Good Evening!</p>;
+      break;
+  }
+  return greeting;
+} 
+  
+  
+  //can also solve with an if statement instead of switch cases
+  //if (currentTime < 12) {
+    //return <p>Good Morning!</p>;
+  //} else if (currentTime >= 12 && currentTime < 18){
+    //return <p>Good Afternoon!</p>;
+  //} else {
+    //return <p>Good Evening!</p>
+ //
+
 
 export default Clock;
